62ffd32bdfa8af900598daddbe67dd4b78639acc
lonewolf12hacker <ibemesin998@gmail.com>
2026-05-21 05:34:56 -0500

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-// Mounts the React engine directly to the HTML structural layer
 const rootElement = document.getElementById('root');
 
-if (!rootElement) {
-  throw new Error('Failed to find the root element. Ensure index.html has <div id="root"></div>');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
